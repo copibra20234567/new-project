c75f78c046ab598c56a6976c0d540ece4ccb5983
copibra20234567 <130291138+copibra20234567@users.noreply.github.com>
2023-04-23 16:30:35 +0300

    new project

---
+++ підкрадулі.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import*
+app = QApplacation([])
+window = QWidget()
+mainline = QVBoxLayout()
+
+widget2 = QPushbutton()
+widget3 = Q
+
+
+
+
+
+
+
+
+
+
+
+
+window.setloyaut(mainline)
+window.show()
+app.exec_()
